540db63a11b47448736b3f51c2e0ead81f5f1c52
Dima <smokotoff@yandex.ru>
2021-06-13 21:58:18 +0300

    Добавил ссылку на схемы

--- Lesson_2/2_1.py
+++ Lesson_2/2_1.py
@@ -1,6 +1,7 @@
 """
 Посчитать четные и нечетные цифры введенного натурального числа.
 Например, если введено число 34560, в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
+https://drive.google.com/file/d/1ObiXYxii8Y_wxDSMySUnztvDFOjxi8vM/view?usp=sharing
 """
 
 tot = number = int(input('Введите число: '))
