ab6ba8b64137d6274134ec8647cce8733208dd7f
Dima <smokotoff@yandex.ru>
2021-06-30 16:55:12 +0300

    Lesson_6

--- Les_6_task_1.py
+++ Les_6_task_1.py
@@ -1,46 +1,30 @@
-"""
-Посчитать, сколько раз встречается определенная цифра в введенной
-последовательности чисел.
-Количество вводимых чисел и цифра, которую необходимо посчитать,
-задаются вводом с клавиатуры.
-"""
-from sys import getsizeof as getsize
+# 64-разрядная ОС / Python 3.9.0
+# Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+import sys
 
-BASE = 10
-sum_ = 0
-num = int(input('Введите количество чисел: '))
-digit = int(input('Какую цифру подсчитать: '))
-count = 0
-for i in range(1, num + 1):
-    ans = int(input(f'Введите число {i}: '))
-    while ans > 0:
-        if ans % BASE == digit:
-            count += 1
-        ans //= BASE  # ans = ans // 10
+# Способ №1
+number = int(input("Введите целое трёхзначное число: "))
+num_1 = number % 10
+num_2 = (number % 100) // 10
+num_3 = number // 100
+sum_ = num_1 + num_2 + num_3
+mult_ = num_1 * num_2 * num_3
+print(f"Сумма цифр введённого Вами числа - {sum_}")
+print(f"Произведение цифр введённого Вами числа - {mult_}")
 
-sum_ += getsize(BASE)
-sum_ += getsize(num)
-sum_ += getsize(digit)
-sum_ += getsize(count)
-sum_ += getsize(ans)
-print(f'Было введено {count} цифр(ы) {digit}')
-print(sum_)
+count = [num_1, num_2, num_3, sum_, mult_]  # самый "топорный" вариант в плане решения задачи и подсчета результата:
 
 
-# sum_ = 0
-# count = 0
-# inp_0 = str(input('Введите последовательность :'))
-# inp_0 = list(inp_0)
-# inp_1 = str(input('Какой символ  будем искать?'))
-#
-# for i in inp_0:
-#     if i == str(inp_1):
-#         count += 1
-# # print(getsize(count))
-# sum_ += getsize(count)
-# # print(getsize(inp_0))
-# sum_ += getsize(inp_0)
-# # print(getsize(inp_1))
-# sum_ += getsize(inp_1)
-# print(f'{count} раз(а) встречается число: {inp_1}')
-# print(sum_)
+# просто сумма из 5-ти вычисляемых значений, которая определяется путем "пробежки" по циклу и суммированию в переменную.
+
+def show(x):
+    result = sys.getsizeof(x)
+    return result
+
+
+total = 0
+for i in count:
+    total += show(i)
+
+print(f'Сумма по всем объектам {total}')
+# Сумма равняется 140. Пять раз взяли по 28.
